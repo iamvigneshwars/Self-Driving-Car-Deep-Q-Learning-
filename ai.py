edcf0a63a181c0c40f64e10cd12a8fd82b143605
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-24 20:33:58 +0530

    Network Architecture

--- ai.py
+++ ai.py
@@ -3,9 +3,18 @@
 import random 
 import os
 import torch 
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as autograd
 from  torch.autograd import Variable
 
+# Architecture of Neural Net
 
+class Network(nn.Module):
+
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_actions = nb_actions
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
