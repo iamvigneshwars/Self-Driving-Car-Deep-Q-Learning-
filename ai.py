de1298fb2771078f4277b2e547e2cac87868848a
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-24 20:22:51 +0530

    Importing the libraries

---
+++ ai.py
@@ -0,0 +1,11 @@
+# Importing the libraries
+
+import random 
+import os
+import torch 
+import torch.nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+from  torch.autograd import Variable
+
+
