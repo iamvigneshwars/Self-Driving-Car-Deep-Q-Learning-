6b2b22ce35df10b896cc40c598dd5e84364892da
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-24 21:33:06 +0530

    Network Architecture forward

--- ai.py
+++ ai.py
@@ -18,3 +18,8 @@ class Network(nn.Module):
         self.nb_actions = nb_actions
         self.fc1 = nn.Linear(input_size, 30)
         self.fc2 = nn.Linear(30, nb_action)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
