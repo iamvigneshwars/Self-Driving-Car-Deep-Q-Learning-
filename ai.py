12ed12de0e16d91d48abe16566de80cde27caf89
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-26 15:45:45 +0530

    Action function

--- ai.py
+++ ai.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as autograd
 from  torch.autograd import Variable
-
+import torch.optim as optim
 # Architecture of Neural Net
 
 class Network(nn.Module):
@@ -27,24 +27,36 @@ class Network(nn.Module):
 # Memory replay
 
 class ReplayMemory(object):
+    
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
-
-
+    
     def push(self, event):
         self.memory.append(event)
         if len(self.memory) > self.capacity:
             del self.memory[0]
     
     def sample(self, batch_size):
-        samples = zip(*random.sample(self.memory, batch_size)
+        samples = zip(*random.sample(self.memory, batch_size))
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
 
 
 # Deep Q Learning Implementation 
 
 class Dqn():
+    
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
 
-    def __init__(self, input_size , nb_action, gamma):
-
+    def select_action(self, state):
+        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
+        action = probs.multinomial(1)
+        return action.data[0,0]
